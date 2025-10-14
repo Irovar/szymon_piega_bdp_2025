556a3b72d558aa7bae92990bdcf7e3f8982d08f2
Szymon Piega <147550891+Irovar@users.noreply.github.com>
2025-10-14 12:09:32 +0200

    Add SQL query to retrieve employee salaries

---
+++ 01_setup/zad5i.sql
@@ -0,0 +1,5 @@
+SELECT p.imie, p.nazwisko, pen.kwota AS pensja
+FROM ksiegowosc.pracownicy p
+JOIN ksiegowosc.wynagrodzenie w ON p.id_pracownika = w.id_pracownika
+JOIN ksiegowosc.pensja pen ON w.id_pensji = pen.id_pensji
+ORDER BY pen.kwota ASC;
