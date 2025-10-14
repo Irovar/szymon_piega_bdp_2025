d4290deba470ea411db885ce2c3c12d8a71db626
Szymon Piega <147550891+Irovar@users.noreply.github.com>
2025-10-14 12:08:06 +0200

    Add SQL query to select employee names with salary range

---
+++ 01_setup/zad5g.sql
@@ -0,0 +1,5 @@
+SELECT p.imie, p.nazwisko
+FROM ksiegowosc.pracownicy p
+JOIN ksiegowosc.wynagrodzenie w ON p.id_pracownika = w.id_pracownika
+JOIN ksiegowosc.pensja pen ON w.id_pensji = pen.id_pensji
+WHERE pen.kwota BETWEEN 1500 AND 3000;
