23146b361a9bae7191136742e86877fb01a4af68
Szymon Piega <147550891+Irovar@users.noreply.github.com>
2025-10-28 12:31:15 +0100

    Add SQL query to compare 2019 and 2018 buildings

--- 03_here/zad1.sql
+++ 03_here/zad1.sql
@@ -1,1 +1,30 @@
+SELECT
+    t2019.geom,
+    t2019.polygon_id
+FROM
+    t2019_kar_buildings AS t2019
+WHERE
+    NOT EXISTS (
+        SELECT 1
+        FROM t2018_kar_buildings AS t2018
+        WHERE ST_Intersects(t2019.geom, t2018.geom)
+    )
 
+UNION ALL
+
+SELECT
+    t2019.geom,
+    t2019.polygon_id
+FROM
+    t2019_kar_buildings AS t2019
+WHERE
+    EXISTS (
+        SELECT 1
+        FROM t2018_kar_buildings AS t2018
+        WHERE ST_Intersects(t2019.geom, t2018.geom)
+    )
+    AND NOT EXISTS (
+        SELECT 1
+        FROM t2018_kar_buildings AS t2018
+        WHERE ST_Equals(t2019.geom, t2018.geom)
+    );
